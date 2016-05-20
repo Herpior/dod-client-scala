40b7b28859998426e5a1f5001278494519e30b04
Herpiö Roope <roope.herpio@gmail.com>
2016-05-20 16:15:05 +0300

    default value for ready button ready for config

--- DoodleOrDieDesktopClient/src/dmodel/ToolModel.scala
+++ DoodleOrDieDesktopClient/src/dmodel/ToolModel.scala
@@ -12,7 +12,7 @@ object ToolModel {
   private var colorpicker = 0
   private var colorpicker2 = 0
   private var state = 0
-  private var ready = true
+  private var ready = Magic.readyDefault
   
   def isReady = this.ready
   def maxsize = if(Magic.authorized)200 else 100
