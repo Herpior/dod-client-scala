ce08b120df6ccf5f8b8305ea32b9669fecc3c345
Herpiö Roope <roope.herpio@gmail.com>
2016-12-05 00:31:31 +0200

    Changed SubmitPanel to use Swing components and functions instead

--- DoodleOrDieDesktopClient/src/view/SubmitPanel.scala
+++ DoodleOrDieDesktopClient/src/view/SubmitPanel.scala
@@ -1,38 +1,69 @@
 package view
 
-import scala.swing.Panel
+import scala.swing.Action
+import scala.swing.Button
+import scala.swing.CheckBox
 import scala.swing.Dimension
-import scala.swing.event._
-import java.awt.BasicStroke
+import scala.swing.FlowPanel
+//import java.awt.BasicStroke
 import java.awt.Color
-import java.awt.Graphics2D
-import java.awt.RenderingHints
+//import java.awt.Graphics2D
+//import java.awt.RenderingHints
 import dmodel.Magic
 import dmodel.ToolModel
-import io.Icons
+//import io.Icons
 
-class SubmitPanel extends Panel {
+class SubmitPanel extends FlowPanel/*(Orientation.Vertical)*/ {
 
   val model = ToolModel
   model.initReady
   
-  this.preferredSize = new Dimension(250, 200)
-  this.minimumSize = preferredSize
-  this.maximumSize = preferredSize
+  //this.preferredSize = new Dimension(250, 200)
+  //this.minimumSize = preferredSize
+  //this.maximumSize = preferredSize
   this.background = Magic.bgColor
   
+  val submitButt = new Button("submit"){
+    this.font = Magic.font20
+    this.preferredSize = new Dimension(200, 50)
+    //this.margin = new Insets(0, 50, 0, 50)
+    //this.border = Swing.EmptyBorder(0, 50, 0, 50)
+    this.background = Color.LIGHT_GRAY//Magic.buttColor
+    this.foreground = Magic.white
+    this.opaque = true
+    this.borderPainted = false
+    this.action = new Action("Submit"){
+      def apply()=submit
+    }
+  }
+  val readyButt = new CheckBox("Ready to submit"){
+    this.font = Magic.font20
+    this.preferredSize = new Dimension(200, 50)
+    this.background = Magic.bgColor
+    this.foreground = Magic.white
+    //this.selectedIcon = Icons.getCheckIcon
+    this.opaque = true
+    this.borderPainted = false
+    this.action = new Action("Ready to submit"){
+      def apply()=clickReady
+    }
+  }
+  this.contents +=   submitButt
+  this.contents +=   readyButt
+  
+  /*
   override def paintComponent(g:Graphics2D){
     super.paintComponent(g)
     g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON)
     var offy = 0
-    
+    /*
     if(model.isReady) g.setColor(Magic.buttColor)
     else g.setColor(Color.LIGHT_GRAY)
     g.fillRoundRect(25, offy, 200, 50, 15, 15)
     g.setColor(Magic.white)
     g.setFont(Magic.font20)
     g.drawString("Submit!", 90, offy+35)
-    
+    */
     offy += 100
     
     g.setStroke(new BasicStroke(2))
@@ -40,23 +71,27 @@ class SubmitPanel extends Panel {
     if(model.isReady) g.drawImage(Icons.getCheck,30,offy+5,null)
     g.setFont(Magic.font20.deriveFont(java.awt.Font.BOLD,18))
     g.drawString("Ready to submit", 70, offy+20)
-  }
+  }*/
   
-  this.listenTo(mouse.clicks)
-  
+  //this.listenTo(mouse.clicks)
+  /*
   this.reactions += {
     case e:MouseReleased =>
       val x = e.point.getX.toInt-25
       val y = e.point.getY.toInt
       //println("submit "+x+" "+y)
-      
+      /*
       if(y<70 && x<200 && x>0){
-        publish(new controller.SubmitEvent)
       }
-      else if(y>=100 && y<130 && x<200 && x>0){
-        model.clickReady
-        this.repaint()
+      else */if(y>=100 && y<130 && x<200 && x>0){
       }
+  }*/
+  private def submit = {
+    publish(new controller.SubmitEvent)
+  }
+  private def clickReady = {
+    model.clickReady
+    submitButt.background = if(model.isReady) Magic.buttColor else Color.LIGHT_GRAY//this.repaint()
   }
   
 }
