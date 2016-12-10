1ef4e7f8af212e0b375601d46635219d1e2ace4c
Herpiö Roope <roope.herpio@gmail.com>
2016-12-10 15:43:37 +0200

    LayerButton uses better text rendering

--- DoodleOrDieDesktopClient/src/view/LayerButton.scala
+++ DoodleOrDieDesktopClient/src/view/LayerButton.scala
@@ -1,6 +1,7 @@
 package view
 
 import scala.swing.Dimension
+import scala.swing.Insets
 import java.awt.Graphics2D
 import java.awt.RenderingHints
 import dmodel.Magic
@@ -11,6 +12,7 @@ class LayerButton extends DodButton {
   this.borderPainted = false
   //this.font = this.font.deriveFont(8f);
   //this.background = Magic.buttColor
+  this.margin = new Insets(-5,-5,-5,-5)
   
   this.contentAreaFilled = false
   this.focusPainted = false
@@ -21,8 +23,6 @@ class LayerButton extends DodButton {
     g.setColor(Magic.buttColor)
     g.fillRoundRect(2, 2, 46, 18, 8, 8)
     g.setColor(Magic.white)
-    val string = this.action.title
-    g.drawString(string, Math.max(25-4*string.length(),3), 15)
-    //super.paintComponent(g)
+    super.paintComponent(g)
   }
 }
