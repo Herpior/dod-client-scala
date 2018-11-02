76831313188f7d74521a838ba59ce287015caf70
Roope Herpiö <roope.herpio@gmail.com>
2018-11-02 12:27:09 +0200

    added test for checking doodles are posted correctly

---
+++ src/test/scala/ExportTest.scala
@@ -0,0 +1,45 @@
+package tests
+
+import dmodel.dpart.BasicLine
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+//import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import java.awt.Color
+import dmodel._
+
+//@RunWith(classOf[JUnitRunner])
+class ExportTest extends FlatSpec with Matchers {
+
+  def fixture =
+    new {
+      val doodleModel = new DoodleModel
+    }
+
+  "DoodleModel.toDodPostJsonStrokes" should "return correctly formatted string" in
+    {
+      val f = fixture
+      val stroke = new BasicLine(Color.decode("#ff9900"),5)
+      stroke.addCoord(Coord(0,0))
+      stroke.addCoord(Coord(1,1))
+      stroke.addCoord(Coord(1,11))
+      stroke.addCoord(Coord(2,22.5))
+      stroke.addCoord(Coord(3,389.5))
+
+      val stroke2 = new BasicLine(Color.decode("#abccee"),5)
+      stroke2.addCoord(Coord(0,0))
+      stroke2.addCoord(Coord(1,1))
+
+      val editStroke = new EditLine(stroke, stroke)
+
+      f.doodleModel.layers.getCurrent.add(stroke)
+      assert (f.doodleModel.toDodPostJsonStrokes.equals("[{\"path\":[0,0,1,1,1,11,2,22.5,3,389.5],\"size\":5,\"color\":\"#f90\"}]"), "doodle with one BasicLine not correctly formatted when posting it")
+      f.doodleModel.layers.getCurrent.add(stroke2)
+      assert (f.doodleModel.toDodPostJsonStrokes.equals("[{\"path\":[0,0,1,1,1,11,2,22.5,3,389.5],\"size\":5,\"color\":\"#f90\"},{\"path\":[0,0,1,1],\"size\":5,\"color\":\"#abccee\"}]"), "doodle with two BasicLines not correctly formatted when posting it")
+      f.doodleModel.layers.getCurrent.add(editStroke)
+      assert (f.doodleModel.toDodPostJsonStrokes.equals("[{\"path\":[0,0,1,1,1,11,2,22.5,3,389.5],\"size\":5,\"color\":\"#f90\"},{\"path\":[0,0,1,1],\"size\":5,\"color\":\"#abccee\"}]"), "doodle with EditLine not correctly formatted when posting it")
+
+    }
+
+}
+
